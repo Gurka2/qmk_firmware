bc2d4ddb48e526d7a480dcd71a01e1049e0c0588
Martin <martin.hogstedt@hotmail.com>
2023-02-05 21:55:35 +0100

    finsished shift layer

--- keyboards/massdrop/alt/keymaps/Gurka2/keymap.c
+++ keyboards/massdrop/alt/keymaps/Gurka2/keymap.c
@@ -20,15 +20,16 @@ enum layers {
 };
 
 /* ============ Keycodes ============ */
-#define MOD MO(MOD_LAYER)
+#define MOD     MO(MOD_LAYER)
+#define SHIFT   LM(SHIFT_LAYER, MOD_LSFT)
 
 
 /* ============ Layout ============ */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_LAYER] = LAYOUT_65_ansi_blocker(
         KC_ESC,  SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    SE_6,    SE_7,    SE_8,    SE_9,    SE_0,    SE_MINS, SE_EQL,  KC_BSPC, KC_DEL,
-        KC_TAB,  SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,    SE_Y,    SE_U,    SE_I,    SE_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        KC_CAPS, SE_A,    SE_S,    SE_D,    SE_F,    SE_G,    SE_H,    SE_J,    SE_K,    SE_L,    SE_SCLN, SE_ADIA,          KC_ENT,  KC_PGUP,
+        KC_TAB,  SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,    SE_Y,    SE_U,    SE_I,    SE_O,    SE_P,    SE_LBRC, SE_RBRC, SE_BSLS, KC_HOME,
+        KC_CAPS, SE_A,    SE_S,    SE_D,    SE_F,    SE_G,    SE_H,    SE_J,    SE_K,    SE_L,    SE_SCLN, SE_QUOT,          KC_ENT,  KC_PGUP,
         KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,    SE_N,    SE_M,    SE_COMM, SE_DOT,  SE_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MOD,   KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -40,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
     [SHIFT_LAYER] = LAYOUT_65_ansi_blocker(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______, SE_EXLM, SE_AT,   SE_HASH, SE_DLR,  SE_PERC, SE_CIRC, SE_AMPR, SE_ASTR, SE_LPRN, SE_RPRN, SE_UNDS, SE_PLUS, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, SE_LCBR, SE_RCBR, SE_PIPE, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, SE_COLN, SE_DQUO,          _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, SE_LABK, SE_RABK, SE_QUES,          _______, _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______
     ),
     /*[CTRL_LAYER] = LAYOUT_65_ansi_blocker(
